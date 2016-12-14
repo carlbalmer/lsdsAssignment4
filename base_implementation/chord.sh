a275c3bf5151cc592059b544d22eddbdeca8ab4a
Carl Balmer <carl.balmer@gmail.com>
2016-12-14 22:36:26 +0100

    changed gitignore

--- base_implementation/chord.sh
+++ base_implementation/chord.sh
@@ -9,9 +9,11 @@ fi
 killall lua
 sleep 1
 
+
+
 for (( n=1;n<=$max;n++ ))
 do
   rm $n.log > /dev/null 2>&1
-  #lua t_chord.lua $n $max > $n.log 2>&1 &
-  lua t_chord.lua $n $max &
+  lua t_chord.lua $n $max > logs/$n.log 2>&1 &
+  #lua t_chord.lua $n $max &
 done
