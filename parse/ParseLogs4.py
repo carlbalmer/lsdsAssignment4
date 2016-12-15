1fe69b41cbd9020cd1e25bbc99a97cb938602f7d
Carl Balmer <carl.balmer@gmail.com>
2016-12-15 17:35:58 +0100

    parser files and plots added

---
+++ parse/ParseLogs4.py
@@ -0,0 +1,26 @@
+import re
+
+inputFile = open("not_optimal_WO_Churn", "r")
+beginning = False
+matches = []
+for line in inputFile:
+    if re.search(r"NotOptimal", line) is not None:
+        split = line.split(" ")
+        matches.append([int(split[2]),float(split[4])])
+inputFile.close()
+
+dict = {}
+
+for i in range(1, 50):
+    dict[i] = {'sum':0, 'count':0}
+
+for match in matches:
+    dict[match[0]]["sum"] += match[1]
+    dict[match[0]]["count"] += 1
+
+print(dict)
+
+
+output_file = open("output.txt", "w")
+for k in dict.keys():
+    output_file.write(str(k)+" "+str(dict[k]["sum"]/dict[k]["count"]) +"\n")
